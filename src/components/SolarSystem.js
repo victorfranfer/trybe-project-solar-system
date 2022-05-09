076c8ad5c35361394ece10f5cb5d3bd34abb1dc2
VictorFranciscoFerreira <victorfranciscoferreira@gmail.com>
2022-05-09 17:47:01 -0300

    requisito 4

--- src/components/SolarSystem.js
+++ src/components/SolarSystem.js
@@ -4,7 +4,7 @@ import Title from './Title';
 class SolarSystem extends React.Component {
   render() {
     return (
-      <div data-testid="solar-system"><Title /></div>
+      <div data-testid="solar-system"><Title headline="Planetas" /></div>
     );
   }
 }

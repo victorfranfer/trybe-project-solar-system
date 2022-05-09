1b42c8f489af9a158cdbc08ddb43af3f80c424b4
VictorFranciscoFerreira <victorfranciscoferreira@gmail.com>
2022-05-09 17:41:33 -0300

    requisito 3 - proptypes

--- src/components/Title.js
+++ src/components/Title.js
@@ -1,11 +1,19 @@
 import React from 'react';
+import PropTypes from 'prop-types';
 
 class Title extends React.Component {
   render() {
+    const { headline } = this.props;
     return (
-      <h2>{this.props.headline}</h2>
+      <header>
+        <h2>{ headline }</h2>
+      </header>
     );
   }
 }
+
+Title.propTypes = {
+  headline: PropTypes.string.isRequired,
+};
 
 export default Title;
